75661d2621301713926a127ef75e3cd61929c90e
Samuel wanza <s.wanza@alustudent.com>
2023-10-06 09:01:54 +0200

    added missing associations and modified some methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  after_save :update_comments_counter
+
+  private
 
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments,
-           def update_posts_counter
-             author.update(posts_counter: author.posts.count)
-           end
+  has_many :comments
+  has_many :likes
+  after_save :update_post_counter
+
+  private
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  public
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
