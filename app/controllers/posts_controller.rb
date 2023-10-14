820a4aef76bb990ebb1b9c3ceae7be2017e6ab19
Samuel wanza <s.wanza@alustudent.com>
2023-10-14 17:41:29 +0200

    added form logic and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_current_user, only: %i[new create]
   def index
     @user = User.find(params[:user_id])
     @user_posts = @user.posts.includes(comments: :user)
@@ -8,5 +9,38 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
     @post_comments = @post.comments.includes(:user)
+  end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.build(post_params)
+    if @post.save
+      redirect_to post_user_url(@user, @post), notice: 'Post created successfully.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+  def like
+    @post = Post.find(params[:id])
+    current_user.like(@post)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def unlike
+    @post = Post.find(params[:id])
+    current_user.unlike(@post)
+    redirect_back(fallback_location: root_path)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,11 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: :user_posts
   get 'users/:user_id/posts', to: 'posts#index',as: :posts_user
+  get '/users/:user_id/posts/new', to: 'posts#new', as: :new_user_post
   get 'users/:user_id/posts/:post_id', to: 'posts#show',as: :post_user
+  get '/users/:user_id/posts/:post_id/comments/new', to: 'comments#new', as: :new_user_post_comment
+  post '/users/:user_id/posts', to: 'posts#create', as: :create_user_post
+  post '/users/:user_id/posts/:post_id/comments', to: 'comments#create', as: :create_user_post_comment
+  post 'posts/:id/like', to: 'posts#like', as: :like_post
+  delete 'posts/:id/unlike', to: 'posts#unlike', as: :unlike_post
 end
